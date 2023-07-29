04517bb6a538b662f1a8cad56f0d6803a222e056
Xavier BALESI <xavier.balesi@linxo.com>
2023-07-29 14:00:17 +0200

    config/nvim: mason add some servers in ensure_installed

--- .config/nvim/lua/configure/mason.lua
+++ .config/nvim/lua/configure/mason.lua
@@ -2,5 +2,13 @@
 -- [[ github: https://github.com/williamboman/mason.nvim ]]
 -- [[ doc: help mason ]]
 
+local servers = {
+    'lua_ls',
+    'bashls',
+    'pyright',
+}
+
 require("mason").setup()
-require("mason-lspconfig").setup()
+require("mason-lspconfig").setup {
+    ensure_installed = servers,
+}
