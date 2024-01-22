0e1622d8a81655b33545e3bc96d0ccaecf11de7f
Xavier BALESI <xavier.balesi@linxo.com>
2024-01-22 22:57:47 +0100

    config/nvim: rework telescope

--- .config/nvim/lua/configure/telescope.lua
+++ .config/nvim/lua/configure/telescope.lua
@@ -2,55 +2,43 @@
 -- [[ github: https://github.com/nvim-telescope/telescope.nvim ]]
 -- [[ doc: help telescope ]]
 
-local vimp = require('vimp')
+local builtin = require("telescope.builtin")
 
--- require('telescope').load_extension('ui-select')
 require('telescope').setup {
-  -- extensions = {
-  --   ["ui-select"] = {
-  --     require("telescope.themes").get_cursor {
-  --       -- even more opts
-  --     },
-  --     codeactions = false,
-  --   }
-  -- },
-  defaults = {
-    layout_config = {
-      -- winblend = 15
+    defaults = {
+        layout_config = {},
+        winblend = 15,
+    },
+    pickers = {
+        find_files = { prompt_prefix = "🔍" },
+        buffers = { prompt_prefix = "🔍" },
+        diagnostics = { initial_mode = 'normal' },
+        live_grep = { prompt_prefix = "🔍" },
+        grep_string = { prompt_prefix = "🔍", initial_mode = 'normal' },
+        current_buffer_fuzzy_find = { prompt_prefix = "🔍" },
     }
-  },
-  -- pickers = {
-  --   registers = {
-  --     theme = "get_cursor",
-  -- winblend = 15,
-  -- initial_mode = 'normal',
-  -- layout_config = { height = 20, width = 65 },
-  --   }
-  -- }
 }
--- require('telescope').load_extension('coc')
 
 function PopRegisters()
-  local registers = require('telescope.builtin').registers
-  local overconf = {
-    winblend = 15,
-    initial_mode = 'normal',
-    layout_config = { height = 20, width = 65 },
-  }
-  local get_cursor = require('telescope.themes').get_cursor
-  local ret = registers(get_cursor(overconf))
-  return ret
+    local registers = require('telescope.builtin').registers
+    local overconf = {
+        initial_mode = 'normal',
+        layout_config = { height = 20, width = 65 },
+    }
+    local get_cursor = require('telescope.themes').get_cursor
+    local ret = registers(get_cursor(overconf))
+    return ret
 end
 
-vimp.nnoremap('ff', ":Telescope find_files prompt_prefix=🔍 winblend=15<cr>")
-vimp.nnoremap('fb', ":Telescope buffers winblend=15<cr>")
-vimp.nnoremap('fc', ":Telescope commands winblend=15<cr>")
-vimp.nnoremap('fd', ":Telescope diagnostics winblend=15<cr>")
-vimp.nnoremap('fh', ":Telescope command_history winblend=15<cr>")
-vimp.nnoremap('fg', ":Telescope live_grep prefix=🔍 winblend=15<cr>")
-vimp.nnoremap('fis', ":Telescope grep_string  prefix=🔍 winblend=15<cr>")
-vimp.nnoremap('fib', ":Telescope current_buffer_fuzzy_find winblend=15<cr>")
-vimp.nnoremap('fk', ":Telescope keymaps winblend=15<cr>")
-vimp.nnoremap('fq', ":Telescope quickfix theme=get_ivy winblend=15<cr>")
-vimp.nnoremap('fl', ":Telescope resume<cr>")
-vimp.nnoremap('fr', PopRegisters)
+vim.keymap.set('n', 'ff', builtin.find_files, {})
+vim.keymap.set('n', 'fb', builtin.buffers, {})
+vim.keymap.set('n', 'fc', builtin.commands, {})
+vim.keymap.set('n', 'fd', builtin.diagnostics, {})
+vim.keymap.set('n', 'fh', builtin.command_history, {})
+vim.keymap.set('n', 'fg', builtin.live_grep, {})
+vim.keymap.set('n', 'fis', builtin.grep_string, {})
+vim.keymap.set('n', 'fib', builtin.current_buffer_fuzzy_find, {})
+vim.keymap.set('n', 'fk', builtin.keymaps, {})
+vim.keymap.set('n', 'fq', builtin.quickfix, {})
+vim.keymap.set('n', 'fl', builtin.resume, {})
+vim.keymap.set('n', 'fr', PopRegisters)
