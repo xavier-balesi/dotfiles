b087ba64b4e6dc10465ca44f5a49422aa4278b5b
Xavier BALESI <xavier.balesi@hps-worldwide.com>
2024-05-01 22:08:52 +0200

    i3/rofi/polybar: add main screen and layout selection

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -8,6 +8,7 @@ polybar-msg cmd quit
 
 # Launch bar1 and bar2
 echo "---" | tee -a /tmp/polybar1.log
+[ -f ~/.config/.current_main_monitor ] && export MONITOR=$(cat ~/.config/.current_main_monitor)
 polybar example 2>&1 | tee -a /tmp/polybar1.log & disown
 
 echo "Bars launched..."
--- playbooks/roles/install-i3wm/files/autostart.sh
+++ playbooks/roles/install-i3wm/files/autostart.sh
@@ -4,6 +4,13 @@ SCRIPT_DIR=$(dirname $(realpath $0))
 SCRIPTS_DIR=${SCRIPT_DIR}/scripts
 
 
+# Set screen layout
+SCREEN_LAYOUT=${HOME}/.config/.current_screenlayout
+[ -f "${SCREEN_LAYOUT}" ] && {
+    layout=$(cat "${SCREEN_LAYOUT}")
+    bash "${layout}"
+}
+
 # Set wallpaper
 CACHE_WALLPAPER=${HOME}/.cache/wallpaper
 DEFAULT_WALLPAPER=${HOME}/.local/share/wallpapers/gruvbox_blame3.jpg
